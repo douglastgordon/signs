2c3761f7457dc345c26e37cb0a415de24509bee7
Douglas Gordon <douglastgordon@gmail.com>
2018-10-22 16:34:03 -0400

    fix underflap bug

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+let currentDownZ = 0
 
 const totalFlaps = values.length
 let currentTopId = 0
@@ -17,10 +18,12 @@ const span = innerHTML => {
   return span
 }
 
+const upperZIndex = idx => Math.floor(5000 / (idx + 1))
+
 const makeSplitFlap = (idx, topValue, bottomValue) => {
   const container = div(["flip-container"])
   container.id = `flap-${idx}`
-  container.style.zIndex = Math.floor(5000 / (idx + 1))
+  container.style.zIndex = upperZIndex(idx)
 
   const flap = div(["flap", "animate"])
   const top = div(["top"])
@@ -34,7 +37,7 @@ const makeSplitFlap = (idx, topValue, bottomValue) => {
   return container
 }
 
-const isLastCard = () => currentTopId === totalFlaps - 1
+const isLastCard = () => currentTopId === totalFlaps - 2
 
 const currentUpFlap = () => document.getElementById(`flap-${currentTopId}`)
 const currentDownIdx = () => (currentTopId - 1 + totalFlaps) % totalFlaps
@@ -42,44 +45,52 @@ const currentDownFlap = () => document.getElementById(`flap-${currentDownIdx()}`
 
 const getFlapByIndex = idx => document.getElementById(`flap-${(idx + totalFlaps) % totalFlaps}`)
 
-const flipDown = idx => {
-  const flap = getFlapByIndex(idx)
-  flap.children[0].style.transform = "rotateX(-180deg)"
-}
 
 const increaseTopId = () => currentTopId = (currentTopId + 1) % totalFlaps
 
 
-const sink = idx => {
+const flipDown = idx => {
   const flap = getFlapByIndex(idx)
-  flap.style.zIndex = idx % (totalFlaps - 1)
+  flap.children[0].classList.add("animate")
+  flap.children[0].style.transform = "rotateX(-180deg)"
 }
 
+const sink = idx => {
+  const flap = getFlapByIndex(idx)
+  flap.style.zIndex = currentDownZ + 1
+  currentDownZ = currentDownZ + 1
+}
+
+const rise = idx => {
+  const flap = getFlapByIndex(idx)
+  flap.style.display = "none"
+  flap.children[0].classList.remove("animate")
+  flap.children[0].style.transform = "rotateX(0deg)"
+  flap.style.zIndex = upperZIndex(idx)
+  flap.style.display = "flex"
+}
+
+
+
+
+
 const flip = () => {
+  const resetFlaps = () => {
+    const currentUpFlap = getFlapByIndex(currentTopId)
+    currentUpFlap.style.zIndex = 5000
+    currentDownZ = 0
+    values.forEach((value, idx) => {
+      if (idx !== currentTopId && idx !== currentTopId - 1) {
+        rise(idx)
+      }
+    })
+  }
 
-  // const currentUpFlapId = `flap-${currentTopId}`
-  // const currentDownFlap = `flap-${currentDownIdx}`
-
-  // const resetFlaps = () => {
-  //   const flapNodes = document.getElementsByClassName("flip-container")
-  //
-  //   const toReset = Array.from(flapNodes).filter(flapNode => (
-  //     flapNode.id !== currentUpFlapId && flapNode !== currentDownFlapId
-  //   ))
-  //
-  //   toReset.forEach(flapNode => {
-  //     const index = flapNode.id.split("-")[1]
-  //     console.log(flapNode)
-  //     flapNode.children[0].style.transform = "rotateX(0deg)"
-  //     flapNode.style.zIndex = Math.floor(5000 / (index + 1))
-  //   })
-  // }
-  //
-  // if(isLastCard()) resetFlaps()
-
-
+  if(isLastCard()) resetFlaps()
   flipDown(currentTopId)
+  console.log(currentTopId)
   sink(currentTopId - 1)
+
   increaseTopId()
 }
 
