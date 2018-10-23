cc34ace3b428da6bb6f3d299ec3325bc8c51ad36
Douglas Gordon <douglastgordon@gmail.com>
2018-10-23 13:47:49 -0400

    fix animation bugs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-const VALUES = DIGITS.concat(LETTERS)
+const PUNCTUATION = [".", ",", ";", ";", "\"", "-"]
+const SPACE = [" "]
+const ALL = [DIGITS, LETTERS, PUNCTUATION, SPACE].flat()
+
+const ALPHANUMERIC = DIGITS.concat(LETTERS)
 
 const makeFlaps = (values, signID) => (
   values.map((value, idx) => ({
@@ -10,27 +14,30 @@ const makeFlaps = (values, signID) => (
   }))
 )
 
-
 const setZs = ({ tops, bottoms }) => {
-  const _setZs = flaps => {
-    flaps.forEach(({ id }, idx) => {
-      const flap = document.getElementById(id)
-      flap.style.zIndex = idx === flaps.length - 1 ? idx + 1 : idx
-    })
-  }
-  _setZs(tops)
-  _setZs(bottoms)
+  const nextUpNode = getFlapContainerNode(tops[tops.length - 2])
+  nextUpNode.style.zIndex = 50
+  const currentUpNode = getFlapContainerNode(tops[tops.length - 1])
+  currentUpNode.style.zIndex = 100
+  const previousDownNode = getFlapContainerNode(bottoms[bottoms.length - 2])
+  previousDownNode.style.zIndex = 0
+  const currentDownNode = getFlapContainerNode(bottoms[bottoms.length - 1])
+  currentDownNode.style.zIndex = 50
 }
 
 
-const getFlapNode = ({ id }) => document.getElementById(id).children[0]
+const getFlapNode = data => getFlapContainerNode(data).children[0]
+const getFlapContainerNode = ({ id }) => document.getElementById(id)
 
 const rotateDown = flap => getFlapNode(flap).style.transform = "rotateX(180deg)"
 const rotateUp = flap => getFlapNode(flap).style.transform = "rotateX(0deg)"
 
 const nextFlap = FLAPDATA => {
   const { tops, bottoms } = FLAPDATA
+  setZs(FLAPDATA)
+
   const currentUp = tops.pop()
+  console.log(currentUp)
   rotateDown(currentUp)
   const currentDown = bottoms.shift()
   rotateUp(currentDown)
@@ -39,7 +46,8 @@ const nextFlap = FLAPDATA => {
   const flipUpBack = () => tops.unshift(currentDown)
   flipDownFront()
   flipUpBack()
-  setTimeout(() => setZs(FLAPDATA), 600)
+
+  // setTimeout(() => setZs(FLAPDATA), 600)
 }
 
 const makeFlapNode = ({id, top, bottom}) => {
@@ -62,6 +70,7 @@ const makeFlapNode = ({id, top, bottom}) => {
   container.id = id
 
   const flap = div(["flap", "animate"])
+  flap.style.zIndex = 0
   const topNode = div(["top"])
   const bottomNode = div(["bottom"])
 
@@ -123,7 +132,7 @@ const flipTo = (FLAPDATA, value) => {
   return true
 }
 
-const createSign = (values, id) => {
+const createSign = (id, values=ALL) => {
   const FLAPDATA = createFlapData(values)
   createDOMFlap(FLAPDATA, id)
   return {
@@ -134,4 +143,4 @@ const createSign = (values, id) => {
 }
 
 
-const sign = createSign(DIGITS, "my-flap")
+const sign = createSign("my-flap", DIGITS)
