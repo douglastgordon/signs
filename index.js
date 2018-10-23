200c639ee83fdee4a3d21add93e040acffa96148
Douglas Gordon <douglastgordon@gmail.com>
2018-10-23 10:36:39 -0400

    fix

--- index.js
+++ index.js
@@ -1,114 +1,92 @@
-const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-let currentDownZ = 0
+const VALUES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const makeFlaps = values => (
+  values.map((value, idx) => ({
+    id: `flap-${idx}`,
+    top: value,
+    bottom: values[(idx + 1) % values.length],
+  }))
+)
 
-const totalFlaps = values.length
-let currentTopId = 0
+const FLAPS = makeFlaps(VALUES)
+const TOPS = FLAPS.slice(0, FLAPS.length / 2).reverse()
+const BOTTOMS = FLAPS.slice(FLAPS.length / 2)
 
-const div = classNames => {
-  const div = document.createElement("div")
-  classNames.forEach(className => {
-    div.classList.add(className)
-  })
-  return div
+
+const setZs = () => {
+  const _setZs = flaps => {
+    flaps.forEach(({ id }, idx) => {
+      const flap = document.getElementById(id)
+      flap.style.zIndex = idx === flaps.length - 1 ? idx + 1 : idx
+    })
+  }
+  _setZs(TOPS)
+  _setZs(BOTTOMS)
 }
 
-const span = innerHTML => {
-  const span = document.createElement("span")
-  span.innerHTML = innerHTML
-  return span
+
+const getFlapNode = ({ id }) => document.getElementById(id).children[0]
+
+const rotateDown = flap => getFlapNode(flap).style.transform = "rotateX(180deg)"
+const rotateUp = flap => getFlapNode(flap).style.transform = "rotateX(0deg)"
+
+const nextFlap = () => {
+  const currentUp = TOPS.pop()
+  rotateDown(currentUp)
+  const currentDown = BOTTOMS.shift()
+  rotateUp(currentDown)
+
+  const flipDownFront = () => BOTTOMS.push(currentUp)
+  const flipUpBack = () => TOPS.unshift(currentDown)
+  flipDownFront()
+  flipUpBack()
+  setTimeout(setZs, 1000)
 }
 
-const upperZIndex = idx => Math.floor(5000 / (idx + 1))
+const makeFlapNode = ({id, top, bottom}) => {
 
-const makeSplitFlap = (idx, topValue, bottomValue) => {
+  const div = classNames => {
+    const div = document.createElement("div")
+    classNames.forEach(className => {
+      div.classList.add(className)
+    })
+    return div
+  }
+
+  const span = innerHTML => {
+    const span = document.createElement("span")
+    span.innerHTML = innerHTML
+    return span
+  }
+
   const container = div(["flip-container"])
-  container.id = `flap-${idx}`
-  container.style.zIndex = upperZIndex(idx)
+  container.id = id
 
   const flap = div(["flap", "animate"])
-  const top = div(["top"])
-  const bottom = div(["bottom"])
+  const topNode = div(["top"])
+  const bottomNode = div(["bottom"])
 
-  top.appendChild(span(topValue))
-  bottom.appendChild(span(bottomValue))
+  topNode.appendChild(span(top))
+  bottomNode.appendChild(span(bottom))
   container.appendChild(flap)
-  flap.appendChild(top)
-  flap.appendChild(bottom)
+  flap.appendChild(topNode)
+  flap.appendChild(bottomNode)
   return container
 }
 
-const isLastCard = () => currentTopId === totalFlaps - 2
 
-const currentUpFlap = () => document.getElementById(`flap-${currentTopId}`)
-const currentDownIdx = () => (currentTopId - 1 + totalFlaps) % totalFlaps
-const currentDownFlap = () => document.getElementById(`flap-${currentDownIdx()}`)
-
-const getFlapByIndex = idx => document.getElementById(`flap-${(idx + totalFlaps) % totalFlaps}`)
-
-
-const increaseTopId = () => currentTopId = (currentTopId + 1) % totalFlaps
-
-
-const flipDown = idx => {
-  const flap = getFlapByIndex(idx)
-  flap.children[0].classList.add("animate")
-  flap.children[0].style.transform = "rotateX(-180deg)"
-}
-
-const sink = idx => {
-  const flap = getFlapByIndex(idx)
-  flap.style.zIndex = currentDownZ + 1
-  currentDownZ = currentDownZ + 1
-}
-
-const rise = idx => {
-  const flap = getFlapByIndex(idx)
-  flap.style.display = "none"
-  flap.children[0].classList.remove("animate")
-  flap.children[0].style.transform = "rotateX(0deg)"
-  flap.style.zIndex = upperZIndex(idx)
-  flap.style.display = "flex"
-}
-
-
-
-
-
-const flip = () => {
-  const resetFlaps = () => {
-    const currentUpFlap = getFlapByIndex(currentTopId)
-    currentUpFlap.style.zIndex = 5000
-    currentDownZ = 0
-    values.forEach((value, idx) => {
-      if (idx !== currentTopId && idx !== currentTopId - 1) {
-        rise(idx)
-      }
-    })
-  }
-
-  if(isLastCard()) resetFlaps()
-  flipDown(currentTopId)
-  console.log(currentTopId)
-  sink(currentTopId - 1)
-
-  increaseTopId()
-}
-
-
-
-document.addEventListener("keydown", e => (e.key === "ArrowDown") && flip())
-
-
+document.addEventListener("keydown", e => (e.key === "ArrowDown") && nextFlap())
 
 {
-  values.forEach((value, idx) => {
-    const topValue = value
-    const bottomValue = values[(idx + 1) % values.length]
-    const flap = makeSplitFlap(idx, topValue, bottomValue)
-    flaps.appendChild(flap)
+  const flapsNode = document.getElementById("flaps")
+  FLAPS.forEach(flapData => {
+    const flap = makeFlapNode(flapData)
+    flapsNode.appendChild(flap)
   });
 
-  const lastFlapIdx = values.length - 1
-  flipDown(lastFlapIdx)
-  sink(lastFlapIdx)
+  setZs()
+
+  BOTTOMS.forEach(({ id }) => {
+    const flapNode = document.getElementById(id).children[0]
+    flapNode.style.transform = "rotateX(180deg)"
+  })
 }
